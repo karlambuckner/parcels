25afee4be398e8903787980cd734158022b8acb5
Karla Buckner and Rebecca McIntosh <karla@>
2017-09-11 14:50:27 -0700

    calculate price based on volume and weight in RUby

--- lib/parcels.rb
+++ lib/parcels.rb
@@ -1,14 +1,20 @@
 class Parcels
-  def initialize(length, width, height)
+  def initialize(length, width, height, weight)
     @length = length
     @width = width
     @height = height
-    # @weight = weight
+    @weight = weight
   end
+
   def volume
-    calculate = @length * @width * @height
+  volume = @length * @width * @height
   end
-  # def cost_to_ship
-  #
-  # end
+
+  def cost_to_ship
+    cost = @weight * volume() / 100
+  if cost <= 5
+    cost = 5
+  end
+  return cost.to_i
+  end
 end
--- spec/parcels_spec.rb
+++ spec/parcels_spec.rb
@@ -2,10 +2,14 @@ require('rspec')
 require('parcels')
 
 describe(Parcels) do
-  describe("#volume") do
-    it('will calculate volume based on user input') do
-      test_volume = Parcels.new(3, 4, 5)
+  describe("#calculation") do
+    it('calculates volume based on user input') do
+      test_volume = Parcels.new(3, 4, 5, 1)
       expect(test_volume.volume()).to(eq(60))
+    end
+    it('calculates price based on volume and weight') do
+      test_price = Parcels.new(3, 4, 5, 10)
+      expect(test_price.cost_to_ship()).to(eq(6))
     end
   end
 end
